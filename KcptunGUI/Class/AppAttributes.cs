98031c1e66dcdb6f3aba75418613eda9ac11346a
Ragnaroks <admin@ragnaroks.org>
2017-01-03 00:39:23 +0800

    `

--- KcptunGUI/Class/AppAttributes.cs
+++ KcptunGUI/Class/AppAttributes.cs
@@ -3,9 +3,10 @@ using System.Windows;
 using System.IO;
 using System.Windows.Interop;
 using System.Windows.Controls;
+using System.ComponentModel;
 
 namespace KcptunGUI.Class {
-    class AppAttributes {
+    class AppAttributes: INotifyPropertyChanged {
         public static readonly String Name = "KcptunGUI";
         public static readonly String Version = Application.ResourceAssembly.GetName().Version.Major + "." + Application.ResourceAssembly.GetName().Version.Minor + "." + Application.ResourceAssembly.GetName().Version.Build;
         public static readonly String Build = Application.ResourceAssembly.GetName().Version.Build.ToString();
@@ -15,7 +16,16 @@ namespace KcptunGUI.Class {
         public static readonly String I18NPath = AppAttributes.ResxPath + "I18N\\";
         public static readonly String ConfigFilePath = AppAttributes.ResxPath + "config.json";
         public static readonly System.Text.UTF8Encoding UTF8EncodingNoBom = new System.Text.UTF8Encoding(false , true);//无BOM编码
-        //public static readonly System.Windows.Media.Imaging.BitmapSource BitmapSource_picture_status_png = Imaging.CreateBitmapSourceFromHBitmap(AppResource.picture_status_png.GetHbitmap() ,IntPtr.Zero ,Int32Rect.Empty ,System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged {
+            add {
+                
+            }
+
+            remove {
+                
+            }
+        }
 
     }
 }
